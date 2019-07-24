7c65f95e94320372a4ff7817e8c47871594e6016
JulianaAbreu <juliana.cerqueira55@gmail.com>
2019-07-24 19:37:19 -0300

    start heroku

--- src/views/components/StandardContent/index.jsx
+++ src/views/components/StandardContent/index.jsx
@@ -4,14 +4,13 @@ import styled from 'styled-components';
 
 const Content = styled.main`
   grid-area: content;
+  padding: 10rem 7rem;
   display: flex;
   justify-content: center;
   text-align: center;
 `;
 
-const StandardContent = ({ children }) => (
-  <Content style={{ padding: '10rem 9rem' }}>{children}</Content>
-);
+const StandardContent = ({ children }) => <Content>{children}</Content>;
 
 StandardContent.propTypes = {
   children: PropTypes.object,
